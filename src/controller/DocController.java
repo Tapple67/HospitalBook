4fb39c1ddeff55be5bc0f4a5421ca6e8f36598af
JUN <guswns020607@naver.com>
2026-08-14 17:21:13 +0900

    jun

--- src/controller/DocController.java
+++ src/controller/DocController.java
@@ -1,5 +1,34 @@
 package controller;
 
+import java.time.DayOfWeek;
+import java.util.ArrayList;
+
+import model.dao.DocDao;
+import model.dto.DocDto;
+import model.dto.PatDto;
+
 public class DocController {
+    private DocController(){}
+    private static final DocController instance = new DocController();
+    public static  DocController getInstance(){ return instance; }
+
+    private DocDao dd = DocDao.getInstance();
+
+    public boolean save(DocDto patDto) {
+        boolean result = dd.save(patDto);
+        return result;
+    }
+
+    public ArrayList<DocDto> findAll( ){
+        ArrayList<DocDto> result = dd.findAll();
+        return result;
+    }
+
+    public boolean update( DocDto DocDto ){
+        return dd.update( DocDto );
+    }
     
+    public boolean delete( int dno ){
+        return dd.delete( dno );
+    }
 }
--- src/model/dto/DocDto.java
+++ src/model/dto/DocDto.java
@@ -1,6 +1,55 @@
 package model.dto;
 
 public class DocDto extends PersonDto{
-    String part;
+    int dno;
+    String dpart;
+
+   
+
+
+    public DocDto() {
+    }
+
+
+    public DocDto(int no, String name, String phone , String part) {
+        super(name, phone);
+        this.dno = no;
+        this.dpart = part;
+    }
+
+
+    public DocDto(int dno, String phone) {
+        super();
+        this.dno = dno;
+    }
+
+
+    public int getNo() {
+        return dno;
+    }
+
+
+    public void setNo(int no) {
+        this.dno = no;
+    }
+
+
+    public String getPart() {
+        return dpart;
+    }
+
+
+    public void setPart(String part) {
+        this.dpart = part;
+    }
+
+
+    @Override
+    public String toString() {
+        return "DocDto [part=" + dpart + ", getName()=" + getName() + ", getPhone()=" + getPhone() + "]";
+    }
+
     
+
+
 }
